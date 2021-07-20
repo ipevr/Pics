28173a50fff0cbf265a207f0951b72d916e468cc
Ingo Peters <ingo.peters2@gmx.de>
2021-07-20 18:05:06 +0200

    Getting data from Unsplash an console.log the result.

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,22 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios
+      .get("https://api.unsplash.com/search/photos", {
+        headers: {
+          Authorization:
+            "Client-ID QB6gURUwJh6f2PL8lbkuOzIWwAXYwvLtWu2vYmR-q7o",
+        },
+        params: {
+          query: term,
+        },
+      })
+      .then((response) => {
+        console.log(response.data.results);
+      });
   }
 
   render() {
