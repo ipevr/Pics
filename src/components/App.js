7f95d7218ae7e7972829dc0e9fc0204e35df4de4
Ingo Peters <ingo.peters2@gmx.de>
2021-07-20 18:07:05 +0200

    Alternative way to get the results.

--- src/components/App.js
+++ src/components/App.js
@@ -3,20 +3,17 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios
-      .get("https://api.unsplash.com/search/photos", {
-        headers: {
-          Authorization:
-            "Client-ID QB6gURUwJh6f2PL8lbkuOzIWwAXYwvLtWu2vYmR-q7o",
-        },
-        params: {
-          query: term,
-        },
-      })
-      .then((response) => {
-        console.log(response.data.results);
-      });
+  async onSearchSubmit(term) {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      headers: {
+        Authorization: "Client-ID QB6gURUwJh6f2PL8lbkuOzIWwAXYwvLtWu2vYmR-q7o",
+      },
+      params: {
+        query: term,
+      },
+    });
+
+    console.log(response.data.results);
   }
 
   render() {
